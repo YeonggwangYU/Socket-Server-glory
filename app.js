f7e2bfe7e86972669639e52f3a68f20d72bb336d
YeonggwangYU <yooglory@naver.com>
2017-04-14 14:28:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var express = require('express');
+var fs = require('fs');
+var socketio = require('socket.io');
+
+var app = express();
+
+app.all('/', function (req, res, next){
+    fs.readFile('socket.chat.html', function(error, result){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(result);
+    });
+});
+
+var server = http.createServer(app).listen(8212, function (){
+    console.log('8212');
+})
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket){
+    socket.on('join', function (result) {
+        console.log('join: ' + socket.room);
+        socket.leave(socket.room);
+        socket.join(result);
+        socket.room = result;
+        //io.sockets.in(socket.room).emit('join', data);
+    });
+
+    socket.on('message', function (result) {
+        console.log(result.inpUserName + ' : ' + result.inpUserMessage + ' : ' + result.date);
+        io.sockets.in(socket.room).emit('message', result);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('disconnect');
+    });
+});
